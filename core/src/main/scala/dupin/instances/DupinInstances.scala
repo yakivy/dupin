ea569d3c6113f7bc0edc34024b91a428ddc5aaa9
Yakiv Yereskovskyi <yakiv.yereskovskyi@gmail.com>
2020-04-28 14:59:57 +0300

    add partial application for element instance

--- core/src/main/scala/dupin/instances/DupinInstances.scala
+++ core/src/main/scala/dupin/instances/DupinInstances.scala
@@ -1,21 +1,26 @@
 package dupin.instances
 
 import cats.Applicative
-import cats.Foldable
 import cats.Traverse
-import cats.syntax.foldable._
-import cats.syntax.traverse._
+import cats.implicits._
 import dupin.core.IndexPart
 import dupin.core.Root
 import dupin.core.Success
 import dupin.core.Validator
+import dupin.instances.DupinInstances.PartialElement
 
 trait DupinInstances {
-    def element[L, R, F[_], RR[_]](
-        validator: Validator[L, R, F])(implicit AF: Applicative[F], TRR: Traverse[RR], RRR: Foldable[RR]
-    ): Validator[L, RR[R], F] = Validator[L, RR[R], F](
-        a => a.mapWithIndex((b, i) => validator.composeP[RR[R]](IndexPart(i.toString) :: Root, _ => b))
-            .foldLeft(Validator[L, RR[R], F](_ => AF.pure(Success(a))))(_ combine _)
-            .f(a)
-    )
+    def element[RR[_]]: PartialElement[RR] = new PartialElement[RR]
+}
+
+object DupinInstances {
+    class PartialElement[RR[_]] {
+        def apply[L, R, F[_]](
+            validator: Validator[L, R, F])(implicit AF: Applicative[F], TRR: Traverse[RR]
+        ): Validator[L, RR[R], F] = Validator[L, RR[R], F](
+            a => a.mapWithIndex((b, i) => validator.composeP[RR[R]](IndexPart(i.toString) :: Root, _ => b))
+                .foldLeft(Validator[L, RR[R], F](_ => AF.pure(Success(a))))(_ combine _)
+                .f(a)
+        )
+    }
 }
